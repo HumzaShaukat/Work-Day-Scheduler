b2422a4a4881df8b0cb7a468534e3d08887dd9e4
Humzas <humzas@umich.edu>
2022-11-14 18:30:25 -0500

    rearranged order of called functions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -45,8 +45,6 @@ function currentSlot() {  //simple function to use day.js output to assign past,
 }
 $(function () {
   var savedEvents = new Array(24);
-  timeCards();
-  currentSlot();
   var dynamicUpdatedTime = setInterval(currentSlot,1000);
   $(".saveBtn").click(function(event) {
     var savedParent = this.parentElement;
@@ -57,5 +55,7 @@ $(function () {
     localStorage.setItem("events", JSON.stringify(savedEvents));
     
   })
+  timeCards();
+  currentSlot();
   $("#currentDay").text(dayjs().format("MMM DD, YYYY"));
 });
